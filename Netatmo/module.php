7bfdd242fec4701e28d9bc86a9db1ddf588ffee2
dapor2000 <dapor@dapor.de>
2015-11-04 14:48:54 +0100

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -1,1 +1,83 @@
- 
+<?
+	class Netatmo extends IPSModule
+	{
+		
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+			$this->RegisterPropertyString("Username", "");
+			$this->RegisterPropertyString("Password", "");
+			$this->RegisterPropertyString("Client_ID", "");
+			$this->RegisterPropertyString("Client_Secret", "");
+		}
+	
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			Initialize();
+
+		}
+		
+		public function Initialize()
+		{
+			$deviceID = $this->CreateInstanceByIdent($this->InstanceID, $this->ReduceGUIDToIdent($_POST['device']), "Device");
+			SetValue($this->CreateVariableByIdent($deviceID, "Latitude", "Latitude", 2), floatval($_POST['latitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Longitude", "Longitude", 2), floatval($_POST['longitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Timestamp", "Timestamp", 1, "~UnixTimestamp"), intval(strtotime($_POST['date'])));
+			SetValue($this->CreateVariableByIdent($deviceID, $this->ReduceGUIDToIdent($_POST['id']), utf8_decode($_POST['name']), 0, "~Presence"), intval($_POST['entry']) > 0);
+			
+		}
+		
+		private function ReduceGUIDToIdent($guid) {
+			return str_replace(Array("{", "-", "}"), "", $guid);
+		}
+		
+
+		private function CreateCategoryByIdent($id, $ident, $name)
+		 {
+			 $cid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($cid === false)
+			 {
+				 $cid = IPS_CreateCategory();
+				 IPS_SetParent($cid, $id);
+				 IPS_SetName($cid, $name);
+				 IPS_SetIdent($cid, $ident);
+			 }
+			 return $cid;
+		}
+		
+		private function CreateVariableByIdent($id, $ident, $name, $type, $profile = "")
+		 {
+			 $vid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($vid === false)
+			 {
+				 $vid = IPS_CreateVariable($type);
+				 IPS_SetParent($vid, $id);
+				 IPS_SetName($vid, $name);
+				 IPS_SetIdent($vid, $ident);
+				 if($profile != "")
+					IPS_SetVariableCustomProfile($vid, $profile);
+			 }
+			 return $vid;
+		}
+		
+		private function CreateInstanceByIdent($id, $ident, $name, $moduleid = "{485D0419-BE97-4548-AA9C-C083EB82E61E}")
+		 {
+			 $iid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($iid === false)
+			 {
+				 $iid = IPS_CreateInstance($moduleid);
+				 IPS_SetParent($iid, $id);
+				 IPS_SetName($iid, $name);
+				 IPS_SetIdent($iid, $ident);
+			 }
+			 return $iid;
+		}
+		
+	
+	}
+?>
