f1532da09f9f81d8480f082ddc54849616fd65df
dapor2000 <dapor@dapor.de>
2015-11-05 14:48:43 +0100

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -79,6 +79,9 @@ require_once(__DIR__ . "/netatmo_api/Clients/NAApiClient.php");
  
     	public function CheckConnection() {
     	global $client;
+    	global $tokens ;     	
+    	global $refresh_token ;
+    	global $access_token ;
     	$this->PrepareConnection();
 	try
 	{
