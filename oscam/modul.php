7fff9ca3d586ff499a6c89ca2c2ca3f9a14c8b03
dapor2000 <dapor@dapor.de>
2015-11-09 17:25:10 +0100

    Update modul.php

--- oscam/modul.php
+++ oscam/modul.php
@@ -1,1 +1,40 @@
+<?php
 
+  
+    class Netatmo extends IPSModule {
+    	
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+  
+            // Selbsterstellter Code
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+        	parent::Create();
+	$this->RegisterPropertyString("username", "");
+	$this->RegisterPropertyString("password", "");
+	$this->RegisterPropertyString("serverurl", "");
+	//$this->RegisterTimer("ReadOscam", 300, 'OSC_getData($_IPS[\'TARGET\']);');
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+      IPS_LogMessage(__CLASS__, __FUNCTION__); //                   
+       IPS_LogMessage('Config', print_r(json_decode(IPS_GetConfiguration($this->InstanceID)), 1));
+ //  	$this->checkConnection();
+   	$this->SetStatus(102);// login OK
+        }
+        
+        
+        
+        
+    }      
+        
+?>
