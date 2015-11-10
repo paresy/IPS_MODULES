9b74526e71e38e5058d165a97564699e0176facb
dapor2000 <dapor@dapor.de>
2015-11-10 13:55:32 +0100

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -263,22 +263,22 @@ $instance_id = $this->CreateCategoryByIdent($instance_id, 'station_name' , $devi
         {
             // Outdoor Module
             case "NAModule1": //	 IPS_LogMessage('NETATMO',"Outdoor");
-             $instance_id = $this->CreateCategoryByIdent($instance_id, 'Outdoor' , 'Outdoor' );
+             $instance_id = $this->CreateCategoryByIdent($instance_id, $device['module_name'] , $device['module_name'] );
                               break;
             //Wind Sensor
             case "NAModule2": 	// IPS_LogMessage('NETATMO',"Wind Sensor");
-               $instance_id = $this->CreateCategoryByIdent($instance_id, 'Wind' , 'Wind Sensor' );
+               $instance_id = $this->CreateCategoryByIdent($instance_id, $device['module_name'] , $device['module_name'] );
                               break;
             //Rain Gauge
             case "NAModule3": //	 IPS_LogMessage('NETATMO',"Rain Gauge");
-              $instance_id = $this->CreateCategoryByIdent($instance_id, 'Rain' , 'Rain Gauge' );
+              $instance_id = $this->CreateCategoryByIdent($instance_id, $device['module_name'] , $device['module_name'] );
                               break;
             //Indoor Module
             case "NAModule4": //	 IPS_LogMessage('NETATMO',"Indoor");
-            $instance_id = $this->CreateCategoryByIdent($instance_id, 'Indoor' , 'Indoor' );
+            $instance_id = $this->CreateCategoryByIdent($instance_id, $device['module_name'] , $device['module_name'] );
                               break;
             case "NAMain" : //	 IPS_LogMessage('NETATMO',"Main device");
-            $instance_id = $this->CreateCategoryByIdent($instance_id, 'Main' , 'Main device' );
+            $instance_id = $this->CreateCategoryByIdent($instance_id, $device['module_name'] , $device['module_name'] );
                             break;
         }
     }
