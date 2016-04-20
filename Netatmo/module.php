af63a0b5b1bc1f35e14c7d9172019adec6d2cb4d
dapor2000 <dapor@dapor.de>
2016-04-20 21:11:00 +0200

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -74,9 +74,10 @@ require_once(__DIR__ . "/netatmo_api/Constants/AppliCommonPublic.php");
 		}
 		catch(Exception $ex)
 		{
+			$this->SetStatus(201);// login failed
 			$this->handleError("An error happened while trying to retrieve your tokens: " .$ex->getMessage()."\n", TRUE);
 	     		// IPS_LogMessage(__CLASS__, "ALL OK !!!!");
-			$this->SetStatus(201);// login failed
+		
      		}
 	}
  
