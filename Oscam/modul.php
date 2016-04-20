922d29bf5308e12991152cc263ce14a28bf5ee07
dapor2000 <dapor@dapor.de>
2016-04-20 14:54:54 +0200

    Update modul.php

--- Oscam/modul.php
+++ Oscam/modul.php
@@ -1,8 +1,8 @@
-﻿<?
-﻿
-﻿
-﻿class Oscam extends IPSModule {
-    	require_once("../public_functions.php");
+<?
+
+
+class Oscam extends IPSModule {
+    
     // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
         public function __construct($InstanceID) {
@@ -181,7 +181,50 @@ $VariID = $this->CreateVariableByIdent($ParentID, $Variname, $Variname, $wert, $
 	 IPS_SetPosition($systemID,  $position);
 }
 
-   
+private function CreateCategoryByIdent($id, $ident, $name)
+ {
+ $cid = @IPS_GetObjectIDByIdent($this->maskUmlaute($ident), $id);
+ if($cid === false)
+ {
+				 $cid = IPS_CreateCategory();
+				 IPS_SetParent($cid, $id);
+				 IPS_SetName($cid, $name);
+				 IPS_SetIdent($cid, $this->maskUmlaute($ident));
+			 }
+			 return $cid;
+		}
+		
+private function CreateVariableByIdent($id, $ident, $name, $value, $type, $profile = "")
+		 {
+			 $vid = @IPS_GetObjectIDByIdent($this->maskUmlaute($ident), $id);
+		 if($vid === false)
+			 {
+				 $vid = IPS_CreateVariable($type);
+				 IPS_SetParent($vid, $id);
+				 IPS_SetName($vid, $name);
+				 IPS_SetIdent($vid, $this->maskUmlaute($ident));
+				 if($profile != "")
+					IPS_SetVariableCustomProfile($vid, $profile);
+			 }
+	
+			SetValue($vid,$value);
+			 return $vid;
+		}
+		
+private function CreateInstanceByIdent($id, $ident, $name, $moduleid = "{24B57877-C24C-4690-8421-B41DCC22BE1B}")
+	 {
+			 $iid = @IPS_GetObjectIDByIdent($this->maskUmlaute($ident), $id);
+			 if($iid === false)
+			 {
+				 $iid = IPS_CreateInstance($moduleid);
+				 IPS_SetParent($iid, $id);
+				 IPS_SetName($iid, $name);
+				 IPS_SetIdent($iid, $this->maskUmlaute($ident));
+			 }
+			 return $iid;
+		}	
+		
+		   
  // CLASS ENDE      
 }      
         
