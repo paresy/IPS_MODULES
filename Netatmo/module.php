bfdae6d5ee85acabea6d4a0f8bf48f0b1b14e75a
dapor2000 <dapor@dapor.de>
2016-04-20 20:55:42 +0200

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -420,7 +420,7 @@ require_once(__DIR__ . "/netatmo_api/Constants/AppliCommonPublic.php");
 		$vid = @IPS_GetObjectIDByIdent($this->maskUmlaute($ident), $id);
 		if($vid === false)
 		{
-			if (preg_match("/^RAIN.*/", $ident) || preg_match("/^sum_rain.*/", $ident)) 
+			if (preg_match("/^Rain.*/", $ident) || preg_match("/^sum_rain.*/", $ident)) 
 			{	
 				$type = 2;
 			}
