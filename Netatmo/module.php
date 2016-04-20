dc920856ff1adf4e6475a32f474cd2088bcdff79
dapor2000 <dapor@dapor.de>
2016-04-20 11:18:12 +0200

    Update module.php

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -1,6 +1,6 @@
 <?
 
-require_once(__DIR__ . "/util.php");  //  Helper Klasse
+require_once(__DIR__ . "/helper/util.php");  //  Helper Klasse
 require_once(__DIR__ . "/netatmo_api/Clients/NAWSApiClient.php");
 require_once(__DIR__ . "/netatmo_api/Constants/AppliCommonPublic.php");
 
